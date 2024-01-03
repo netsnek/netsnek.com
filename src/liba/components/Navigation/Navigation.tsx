66263edab176f5f880a7f996a0d815610e29db4d
Florian Kleber <kleber@snek.at>
2024-01-03 00:10:23 +0000

    feat: update header navigation and hamburger menu
    - Implement new navigation with responsive design adjustments
    - Restructure header component and adjust initial heights for animation
    - Add new state management for hamburger menu class toggling
    - Add new color palette for brand and update in theme
    - Refactor HamburgerMenuIcon with new style prop structure and additional styles
    - Include new links and follow icons in mobile drawer
    - Update various components for better alignment and layout in mobile view

--- src/liba/components/Navigation/Navigation.tsx
+++ src/liba/components/Navigation/Navigation.tsx
@@ -1,50 +1,338 @@
-import {Box, Stack} from '@chakra-ui/react'
-import {FC} from 'react'
+import {
+  Text,
+  Box,
+  Flex,
+  HStack,
+  Stack,
+  VStack,
+  useDisclosure,
+  Link,
+  IconButton,
+  Grid,
+  Divider,
+  CloseButton,
+  Container,
+  SimpleGrid,
+  Icon,
+  Spacer,
+  Button,
+  LinkOverlay,
+  LinkBox,
+  useColorModeValue,
+  Tooltip
+} from '@chakra-ui/react'
+import {FC, useState} from 'react'
 
 import {Navbar} from './Navbar'
 
 import {useContactModal} from '../../services/contact'
 import TopNav from './TopNav'
 import {useAuthenticationContext} from '@atsnek/jaen'
+import {CloseIcon, HamburgerIcon} from '@chakra-ui/icons'
+import {FaTwitter} from '@react-icons/all-files/fa/FaTwitter'
+import {FaGithub} from '@react-icons/all-files/fa/FaGithub'
+import {FaInstagram} from '@react-icons/all-files/fa/FaInstagram'
+import {FaFacebook} from '@react-icons/all-files/fa/FaFacebook'
+import {FiSearch} from '@react-icons/all-files/fi/FiSearch'
+import Netsnek from '../../../gatsby-plugin-jaen/components/Netsnek'
+import Logo from '../../../gatsby-plugin-jaen/components/Logo'
+import HamburgerMenuIcon, {
+  THamburgerMenuIconStylerProps
+} from '../../../shared/components/HamburgerMenuIcon'
 
-interface INavigationProps {
+interface IHeaderProps {
   path: string
+  hamburgerIconProps?: THamburgerMenuIconStylerProps
 }
 
-const Navigation: FC<INavigationProps> = ({path}) => {
+const Header: FC<IHeaderProps> = ({path, hamburgerIconProps}) => {
+  hamburgerIconProps = {
+    color: 'red.500'
+  }
   const contactModal = useContactModal()
-  const {isAuthenticated} = useAuthenticationContext()
   const handleOnContactClick = () => {
     contactModal.onOpen({
       meta: {}
     })
   }
+  const {isOpen, onOpen, onClose} = useDisclosure()
+  const [hamburgerClass, setHamburgerClass] = useState('')
+
+  const openDrawer = () => {
+    setHamburgerClass('open')
+    onOpen()
+  }
+
+  const closeDrawer = () => {
+    setHamburgerClass('')
+    onClose()
+  }
+
+  const toggleMobileMenu = () => {
+    if (hamburgerClass === 'open') closeDrawer()
+    else openDrawer()
+  }
 
   return (
-    <>
-      <TopNav />
+    <Box
+      pos="relative"
+      overflow="hidden"
+      minH="100px"
+      height={isOpen ? '100vh' : '100px'}
+      transition="height 0.3s ease-in-out">
       <Box
-        as="nav"
-        zIndex="5"
-        pos={'sticky'}
-        top={(isAuthenticated ? '4rem' : '0')}
-        // mt={mode === 'store' ? (isAuthenticated ? '4rem' : '0') : undefined}
-        //bg="rgba(244,248,250,.8)"
-        bg="rgba(255,255,255,.8)"
-        //bg="rgba(186, 224, 226,.9)"
-        borderBottomRadius="2xl"
-        //borderBottom={'1px solid'}
-        //borderColor={'brand.500'}
-        backdropFilter={'blur(7px)'}>
-        <Stack spacing="0">
-          <Navbar
-            pathname={path}
-            onContactClick={handleOnContactClick}
-          />
-        </Stack>
+        pos="relative"
+        bg="#0A0A0A"
+        transition="height 0.3s ease-in-out"
+        height={isOpen ? '100vh' : '0'} // Adjust according to your header's initial height
+        width="100%"
+        overflow="hidden">
+        <Container maxW="8xl">
+          <VStack
+            px="0"
+            pt="4"
+            pb="8"
+            spacing="4"
+            align="start"
+            h="90vh"
+            mt="15vh">
+            <SimpleGrid
+              fontSize="xxx-large"
+              flex="1.5"
+              w="full"
+              columns={{base: 1, md: 2}}>
+              <LinkBox
+                flex="1"
+                display="flex"
+                pl="16"
+                alignItems="center"
+                borderWidth="1px"
+                borderStyle="solid"
+                borderRight={{base: '0', md: '1px'}}
+                borderLeft="0"
+                borderBottom="0"
+                borderColor={useColorModeValue('gray.900', 'gray.600')}>
+                <LinkOverlay href="/work" color="white">
+                  Our Work
+                </LinkOverlay>
+              </LinkBox>
+              <LinkBox
+                flex="1"
+                display="flex"
+                pl="16"
+                alignItems="center"
+                borderWidth="1px"
+                borderStyle="solid"
+                borderBottom="0"
+                borderRight="0"
+                borderLeft="0"
+                borderColor={useColorModeValue('gray.900', 'gray.600')}>
+                <LinkOverlay href="/about" color="white">
+                  About Us
+                </LinkOverlay>
+              </LinkBox>
+              <LinkBox
+                flex="1"
+                display="flex"
+                pl="16"
+                alignItems="center"
+                borderWidth="1px"
+                borderStyle="solid"
+                borderRight={{base: '0', md: '1px'}}
+                borderLeft="0"
+                borderColor={useColorModeValue('gray.900', 'gray.600')}>
+                <LinkOverlay href="/process" color="white">
+                  Our Process
+                </LinkOverlay>
+              </LinkBox>
+              <LinkBox
+                flex="1"
+                display="flex"
+                pl="16"
+                alignItems="center"
+                borderWidth="1px"
+                borderRight="0"
+                borderLeft="0"
+                borderStyle="solid"
+                borderColor={useColorModeValue('gray.900', 'gray.600')}>
+                <LinkOverlay href="/blog" color="white">
+                  Blog
+                </LinkOverlay>
+              </LinkBox>
+              <Box pt="16" pl="16">
+                <Text color="white" fontWeight="bold" fontSize="lg" pb="4">
+                  Our offices
+                </Text>
+                <Text color="gray.400" fontSize="md">
+                  Vienna
+                </Text>
+                <Text color="gray.400" fontSize="md">
+                  Löwengasse 28
+                </Text>
+                <Text color="gray.400" fontSize="md">
+                  1030 Vienna, Austria
+                </Text>
+              </Box>
+              <Box pt="16" pl="16">
+                <Text color="white" fontWeight="bold" fontSize="lg">
+                  Follow us
+                </Text>
+                <HStack spacing="12">
+                  <Link href="https://facebook.com" isExternal>
+                    <Icon as={FaFacebook} boxSize="6" color="white" />
+                  </Link>
+                  <Link href="https://instagram.com" isExternal>
+                    <Icon as={FaInstagram} boxSize="6" color="white" />
+                  </Link>
+                  <Link href="https://twitter.com" isExternal>
+                    <Icon as={FaTwitter} boxSize="6" color="white" />
+                  </Link>
+                  <Link href="https://github.com" isExternal>
+                    <Icon as={FaGithub} boxSize="6" color="white" />
+                  </Link>
+                </HStack>
+              </Box>
+            </SimpleGrid>
+          </VStack>
+        </Container>
       </Box>
-    </>
+      <Box pos="absolute" top="0" left="0" w="100%">
+        <Container maxW="8xl">
+          <HStack
+            px={{base: '4', md: '8'}}
+            py={{base: '2', md: '4'}}
+            justifyContent="space-between"
+            alignItems="center">
+            <Link href="/" aria-label="Home">
+              <Logo height="75px" color="black" />
+            </Link>
+            <Flex alignItems="center">
+              <Button
+                mr={4}
+                filter="drop-shadow(1px 2px 2px rgb(0 0 0 / 0.1))"
+                borderRadius={'full'}
+                onClick={handleOnContactClick}
+                fontSize="sm"
+                fontWeight="semibold"
+                color="white"
+                display={{base: 'none', md: 'block'}}
+              >
+                Contact us
+              </Button>
+              <Tooltip label="Artikel-Suche">
+                <IconButton
+                  mr={4}
+                  borderRadius={'full'}
+                  filter="drop-shadow(1px 2px 2px rgb(0 0 0 / 0.1))"
+                  w={{base: '100%', md: 'unset'}}
+                  aria-label="Search"
+                  fontWeight={'bold'}
+                  icon={<FiSearch />}
+                  onClick={toggleMobileMenu}
+                />
+              </Tooltip>
+              <IconButton
+                icon={
+                  <HamburgerMenuIcon
+                    handleClick={toggleMobileMenu}
+                    wrapperProps={{className: hamburgerClass}}
+                    iconProps={{
+                      // ...hamburgerIconProps,
+                      //boxSize: '6',
+                      //boxSize: '100%',
+                      backgroundColor: 'white'
+                    }}
+                  />
+                }
+                filter="drop-shadow(1px 2px 2px rgb(0 0 0 / 0.1))"
+                borderRadius={'full'}
+                //variant="ghost"
+                fontWeight={'bold'}
+                aria-label={isOpen ? 'Close menu' : 'Open menu'}
+                onClick={toggleMobileMenu}
+              />
+            </Flex>
+          </HStack>
+        </Container>
+      </Box>
+      <Box
+        pointerEvents="none"
+        pos="absolute"
+        top="0"
+        left="0"
+        transition="height 0.3s ease-in-out"
+        h={isOpen ? '100vh' : '0'} // Adjust according to your header's initial height
+        w="100%"
+        overflow="hidden"
+        style={{clip: 'rect(0, auto, auto, 0)'}}>
+        <Container maxW="8xl">
+          <HStack
+            px={{base: '4', md: '8'}}
+            py={{base: '2', md: '4'}}
+            justifyContent="space-between"
+            alignItems="center">
+            <Link href="/" aria-label="Home">
+              <Logo height="75px" color="white" />
+            </Link>
+            <Flex alignItems="center">
+              <Button
+                mr={4}
+                borderRadius={'full'}
+                filter="drop-shadow(1px 2px 2px rgb(0 0 0 / 0.1))"
+                //variant="ghost"
+                onClick={handleOnContactClick}
+                fontSize="sm"
+                fontWeight="semibold"
+                bg="white"
+                color="black"
+                display={{base: 'none', md: 'block'}}
+                _hover={{bg: 'gray.200'}}
+                >
+                Contact us
+              </Button>
+              <Tooltip label="Artikel-Suche">
+                <IconButton
+                  mr={4}
+                  borderRadius={'full'}
+                  filter="drop-shadow(1px 2px 2px rgb(0 0 0 / 0.1))"
+                  w={{base: '100%', md: 'unset'}}
+                  aria-label="Search"
+                  fontWeight={'bold'}
+                  bg="white"
+                  color="black"
+                  icon={<FiSearch />}
+                  onClick={toggleMobileMenu}
+                />
+              </Tooltip>
+              <IconButton
+                icon={
+                  <HamburgerMenuIcon
+                    // handleClick={toggleMobileMenu}
+                    wrapperProps={{className: hamburgerClass}}
+                    iconProps={{
+                      // ...hamburgerIconProps,
+                      //boxSize: '6',
+                      //boxSize: '100%',
+                      backgroundColor: 'black'
+                    }}
+                  />
+                }
+                filter="drop-shadow(1px 2px 2px rgb(0 0 0 / 0.1))"
+                borderRadius={'full'}
+                //variant="ghost"
+                fontWeight={'bold'}
+                aria-label={isOpen ? 'Close menu' : 'Open menu'}
+                onClick={toggleMobileMenu}
+                bg={'white'}
+                color={'black'}
+                _hover={{bg: 'gray.800'}}
+              />
+            </Flex>
+          </HStack>
+        </Container>
+      </Box>
+    </Box>
   )
 }
 
-export default Navigation
+export default Header
--- src/shared/components/HamburgerMenuIcon.tsx
+++ src/shared/components/HamburgerMenuIcon.tsx
@@ -1,12 +1,12 @@
-import { Box, BoxProps } from '@chakra-ui/react';
-import { FC, useState } from 'react';
+import {Box, BoxProps} from '@chakra-ui/react'
+import {FC, useState} from 'react'
 
-export type THamburgerMenuIconStylerProps = BoxProps;
+export type THamburgerMenuIconStylerProps = BoxProps
 
 interface IHamburgerMenuIconProps {
-  handleClick?: (isOpen: boolean) => void;
-  wrapperProps?: BoxProps;
-  iconProps?: BoxProps;
+  handleClick?: (isOpen: boolean) => void
+  wrapperProps?: BoxProps
+  iconProps?: BoxProps
 }
 
 /**
@@ -21,14 +21,14 @@ const HamburgerMenuIcon: FC<IHamburgerMenuIconProps> = ({
     __css: {
       '&.open': {
         '& > div:nth-of-type(1)': {
-          top: '8px',
+          top: '50%',
           transform: 'rotate(45deg)'
         },
         '& > div:nth-of-type(2)': {
           opacity: 0
         },
         '& > div:nth-of-type(3)': {
-          top: '8px',
+          top: '50%',
           transform: 'rotate(-45deg)'
         }
       },
@@ -39,39 +39,52 @@ const HamburgerMenuIcon: FC<IHamburgerMenuIconProps> = ({
       ...wrapperProps?.__css
     },
     ...wrapperProps
-  };
+  }
 
   return (
-    <Box position="relative" boxSize="11px" onClick={handleClick} {...props}>
+    <Box
+      position="relative"
+      rounded="full"
+      boxSize="100%"
+      //bg="brand.500"
+      // _hover={{
+      //   backgroundColor: 'gray.500'
+      // }}
+      onClick={handleClick}
+      {...props}
+      >
       <Box
         position="absolute"
-        top={0}
-        w="11px"
-        h="2px"
+        top="34%"
+        left="25%"
+        w="50%"
+        h="4%"
         backgroundColor="topNav.mobile.hamburger.bgColor"
         borderRadius="full"
         {...iconProps}
       />
       <Box
         position="absolute"
-        top="5px"
-        w="11px"
-        h="2px"
+        top="49%"
+        left="25%"
+        w="50%"
+        h="4%"
         backgroundColor="topNav.mobile.hamburger.bgColor"
         borderRadius="full"
         {...iconProps}
       />
       <Box
         position="absolute"
-        top="10px"
-        w="11px"
-        h="2px"
+        top="64%"
+        left="25%"
+        w="50%"
+        h="4%"
         backgroundColor="topNav.mobile.hamburger.bgColor"
         borderRadius="full"
         {...iconProps}
       />
     </Box>
-  );
-};
+  )
+}
 
-export default HamburgerMenuIcon;
+export default HamburgerMenuIcon
