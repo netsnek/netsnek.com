e2b9fc4e1e1f0d8255c688fb7ff7eddfad22f289
Florian Kleber <kleber@snek.at>
2024-07-26 11:24:38 +0000

    feat: remove unused code and files

--- src/gatsby-plugin-jaen/components/Layout.tsx
+++ src/gatsby-plugin-jaen/components/Layout.tsx
@@ -14,15 +14,15 @@ const Layout: React.FC<LayoutProps> = ({ children, pageProps }) => {
 
   const jaenFrame = useJaenFrameMenuContext();
 
-  useEffect(() => {
-    jaenFrame.extendAddMenu({
-      experimentNew: {
-        label: 'New experiment',
-        icon: FaFlask,
-        path: '/new/experiment'
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   jaenFrame.extendAddMenu({
+  //     experimentNew: {
+  //       label: 'New experiment',
+  //       icon: FaFlask,
+  //       path: '/new/experiment'
+  //     }
+  //   });
+  // }, []);
 
   const isDocs = docsPaths.some(docsPath => path.startsWith(docsPath));
 
