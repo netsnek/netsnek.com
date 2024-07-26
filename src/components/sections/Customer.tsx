67bc7c5f308a807d0218fcaea2ddbf33baf885f5
Florian Kleber <kleber@snek.at>
2024-07-26 11:06:13 +0000

    feat: update customer section with dynamic content
    Updated the "Customer" section to use dynamic content from the Jaen CMS. Added `Field.Text` components to replace static headings and text. This allows for easier customization and localization of the customer section.

--- src/components/sections/Customer.tsx
+++ src/components/sections/Customer.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem, Heading, Text, Button, chakra, Container } from '@chakra-ui/react';
 import { useContactModal } from '../../services/contact';
+import { Field } from 'jaen';
 
 const Customer = () => {
   const contactModal = useContactModal()
@@ -56,53 +57,83 @@ const Customer = () => {
         alignItems="start"
       >
         <GridItem colSpan={{ base: 1, md: 2, lg: 3 }} textAlign="center" area="heading-main" mb={4}>
-          <Heading as="h2" size="xl" mt={4} fontWeight="500">
+        <Field.Text
+          //mt={{ base: '20 !important', md: '0' }}
+          //mb="8"
+          as={Heading}
+          size="xl"
+          mt={4}
+          fontWeight="500"
+          name="customertopheading1"
+          defaultValue="Meine Kunden"
+        />
+          {/* <Heading as="h2" size="xl" mt={4} fontWeight="500">
             Meine Kunden
             <chakra.span color="brand.500">.</chakra.span>
-          </Heading>
+          </Heading> */}
         </GridItem>
 
         {/* Pair 1 */}
         <GridItem area="heading1">
-          <Heading as="h3" size="md">
-            Du bist Unternehmer und möchtest den ganzen Tag über konzentriert Entscheidungen treffen?
-          </Heading>
+          <Field.Text
+            name="customerheading1"
+            as={Heading}
+            color="brand.900"
+            size="md"
+            defaultValue={'Du bist Unternehmer und möchtest den ganzen Tag über konzentriert Entscheidungen treffen?'}
+          />
         </GridItem>
         <GridItem area="text1" mb={4}>
-          <Text>
-            Wir lernen gemeinsam kennen, wie dein Körper auf unterschiedliche Lebensmittel reagiert. Auf was solltest du beim schnellen Mittagessen achten? Welche Essgewohnheiten bringen dir Energie, welche mindern sie? Mit welchen Lebensmitteln gelingt eine gesunde, selbstgekochte Mahlzeit, wenns nur 10 Minuten dauern darf?
-          </Text>
+          <Field.Text
+            name="customertext1"
+            // color="gray.800"
+            defaultValue={'Wir lernen gemeinsam kennen, wie dein Körper auf unterschiedliche Lebensmittel reagiert. Auf was solltest du beim schnellen Mittagessen achten? Welche Essgewohnheiten bringen dir Energie, welche mindern sie? Mit welchen Lebensmitteln gelingt eine gesunde, selbstgekochte Mahlzeit, wenns nur 10 Minuten dauern darf?'}
+          />
         </GridItem>
 
         {/* Pair 2 */}
         <GridItem area="heading2">
-          <Heading as="h3" size="md">
-            Du bist Mutter/Vater und möchtest mehr Zeit für deine Familie schaffen?
-          </Heading>
+          <Field.Text
+            name="customerheading2"
+            as={Heading}
+            color="brand.900"
+            size="md"
+            defaultValue={'Du bist Mutter/Vater und möchtest mehr Zeit für deine Familie schaffen?'}
+          />
         </GridItem>
         <GridItem area="text2" mb={4}>
-          <Text>
-            Wichtig ist, dass jeder gerne bei der Ernährungsweise mitmacht und dass das Kochen keine Stunden benötigt. Wie kann man sich ausführlich belohnen, ohne gesunde Routinen zu brechen? Ziel ist es, dass ihr auch abends noch Energie für Aktivitäten oder für gemeinsame Hausaufgaben habt.
-          </Text>
+          <Field.Text
+            name="customertext2"
+            // color="gray.800"
+            defaultValue={'Wichtig ist, dass jeder gerne bei der Ernährungsweise mitmacht und dass das Kochen keine Stunden benötigt. Wie kann man sich ausführlich belohnen, ohne gesunde Routinen zu brechen? Ziel ist es, dass ihr auch abends noch Energie für Aktivitäten oder für gemeinsame Hausaufgaben habt.'}
+          />
         </GridItem>
 
         {/* Pair 3 */}
         <GridItem area="heading3">
-          <Heading as="h3" size="md">
-            Du arbeitest körperlich und hast am Abend keine Energie mehr für deine persönlichen Bedürfnisse?
-          </Heading>
+          <Field.Text
+            name="customerheading3"
+            as={Heading}
+            color="brand.900"
+            size="md"
+            defaultValue={'Du arbeitest körperlich und hast am Abend keine Energie mehr für deine persönlichen Bedürfnisse?'}
+          />
         </GridItem>
         <GridItem area="text3" mb={4}>
-          <Text>
-            Von was profitiert dein Körper? Woher nimmt dein Körper die Energie? Wir finden heraus, welche Speisen nicht nur ausreichend Energie für den Alltag bringen, sondern auch deine Gesundheit nachhaltig verbessern.
-          </Text>
+          <Field.Text
+            name="customertext3"
+            // color="gray.800"
+            defaultValue={'Von was profitiert dein Körper? Woher nimmt dein Körper die Energie? Wir finden heraus, welche Speisen nicht nur ausreichend Energie für den Alltag bringen, sondern auch deine Gesundheit nachhaltig verbessern.'}
+          />
         </GridItem>
 
         {/* Bottom Text */}
         <GridItem colSpan={{ base: 1, md: 2, lg: 3 }} textAlign="center" area="bottom-text" mb={4}>
-          <Text>
-            Jeder Mensch profitiert von mehr Energie und klareren Gedanken. Wir erhöhen deine Lebensqualität, indem du deinen Körper besser zu verstehen lernst.
-          </Text>
+        <Field.Text
+            name="customerbottomtext"
+            // color="gray.800"
+            defaultValue={'Jeder Mensch profitiert von mehr Energie und klareren Gedanken. Wir erhöhen deine Lebensqualität, indem du deinen Körper besser zu verstehen lernst.'}
+          />
         </GridItem>
 
         {/* Bottom Button */}
