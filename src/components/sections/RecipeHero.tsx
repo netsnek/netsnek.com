cd0bfa8f00a4483bec8f99de0a4f5f42c6357c92
Florian Kleber <kleber@snek.at>
2024-08-04 07:58:59 +0000

    feat: Update RecipeHero component with new image layout
    Update the RecipeHero component in RecipeHero.tsx to include a new image layout. This improves the visual presentation of the recipe hero section on the page.

--- src/components/sections/RecipeHero.tsx
+++ src/components/sections/RecipeHero.tsx
@@ -108,7 +108,71 @@ const RecipeHero: FC<RecipeHeroProps> = ({ defaultHeading, defaultLead }) => {
         <Box as={FadeIn} position="relative" gridArea="image">
           {/* <AspectRatio ratio={1 / 1.04} w="full" h="auto" maxH="700px"> */}
           <Box position="relative" w="full" h="full">
-            <Box
+            <VStack flex="1">
+              <Grid placeItems="center" pos="relative">
+
+
+                <AspectRatio
+                  ratio={1}
+                  pos="absolute"
+                  boxShadow="dark"
+                  overflow="hidden"
+                  isolation="isolate"
+                  w="75%"
+                //borderRadius="full"
+                >
+                  <Field.Image name="RecipeHeroimage" />
+                </AspectRatio>
+                <chakra.svg
+                  zIndex={100}
+                  pointerEvents="none"
+                  w="30rem"
+                  maxW="100%"
+                  h="auto"
+                  viewBox="0 0 809 678" // Adjusted for padding
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  preserveAspectRatio="xMidYMid meet"
+                  sx={{
+                    '#Selection': {
+                      fill: '#f6f8fa'
+                    }
+                  }}
+                >
+                  <path id="Auswahl #1"
+                    fill="#dee9ec"
+                    d="M -208.00,-158.00
+           C -208.00,-158.00 1010.00,-158.00 1010.00,-158.00
+             1010.00,-158.00 1010.00,798.00 1010.00,798.00
+             1010.00,798.00 -208.00,798.00 -208.00,798.00
+             -208.00,798.00 -208.00,-158.00 -208.00,-158.00 Z
+           M 332.00,82.14
+           C 326.61,83.06 326.37,82.99 321.00,83.00
+             321.00,83.00 283.00,86.28 283.00,86.28
+             227.50,93.59 167.95,109.50 132.23,156.00
+             110.00,184.95 99.43,221.01 96.91,257.00
+             96.91,257.00 96.00,267.00 96.00,267.00
+             95.63,298.85 99.91,328.62 109.74,359.00
+             137.10,443.50 207.72,513.22 291.00,542.69
+             311.58,549.97 344.28,557.74 366.00,558.00
+             366.00,558.00 383.00,559.00 383.00,559.00
+             383.00,559.00 405.00,558.00 405.00,558.00
+             417.22,557.98 438.63,554.81 451.00,552.58
+             525.82,539.06 601.24,497.15 647.11,436.00
+             663.86,413.67 675.29,391.81 685.60,366.00
+             695.48,341.28 703.69,307.64 704.00,281.00
+             704.00,281.00 704.99,269.00 704.99,269.00
+             704.99,269.00 704.09,257.00 704.09,257.00
+             701.58,220.30 693.91,189.86 669.56,161.00
+             649.87,137.66 621.19,122.21 593.00,111.58
+             548.88,94.93 500.80,88.13 454.00,84.91
+             454.00,84.91 423.00,83.00 423.00,83.00
+             423.00,83.00 407.00,82.14 407.00,82.14
+             407.00,82.14 332.00,82.14 332.00,82.14 Z" />
+                </chakra.svg>
+              </Grid>
+            </VStack>
+            {/* <Box
               defaultValue="/images/sweets-image-3.png"
               name="RecipeHeroimage"
               as={Field.Image}
@@ -121,7 +185,7 @@ const RecipeHero: FC<RecipeHeroProps> = ({ defaultHeading, defaultLead }) => {
                 //borderRadius: 'md',
                 //filter: 'drop-shadow(1px 2px 2px rgb(0 0 0 / 0.1))'
               }}
-            />
+            /> */}
             {/* <Image
                 src="images/header-portrait-image.png"
                 alt="RecipeHero image"
