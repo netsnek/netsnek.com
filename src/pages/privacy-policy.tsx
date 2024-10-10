e5db15e285292fcb5c5d89ae645895b7aeaa6ca2
Florian Kleber <kleber@snek.at>
2024-10-10 05:44:47 +0000

    refactor: update privacy policy page
     - Add missing import for Field component
     - Wrap page content in a Box component
     - Replace deprecated navigate function with a link to the
       privacy policy document
     - Rename the page component to PrivacyPolicyPage

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -1,19 +1,18 @@
-import { PageConfig, PageProps } from 'jaen';
+import { Field, PageConfig, PageProps } from 'jaen';
 
+import { Box } from '@chakra-ui/react';
 import { graphql, navigate } from 'gatsby';
 import * as React from 'react';
 
-const Page: React.FC<PageProps> = () => {
-  React.useEffect(() => {
-    navigate(
-      'https://dsba.univie.ac.at/fileadmin/user_upload/p_dsba/datenschutzerklaerung_websites_V04_26062020_EN.pdf'
+const PrivacyPolicyPage: React.FC<PageProps> = () => {
+    return (
+      <Box as="main">
+        <Field.Editor name="privacy-policy" />
+      </Box>
     );
-  }, []);
-
-  return null;
-};
-
-export default Page;
+  };
+  
+export default PrivacyPolicyPage;
 
 export const pageConfig: PageConfig = {
   label: 'Privacy Policy',
